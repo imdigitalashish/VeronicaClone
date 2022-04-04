3d43dfd3bb2a12c0bc8f6c3df838aae00af91bbe
Ashish Kumar Verma <imdigitalashish@gmail.com>
2022-04-04 22:53:13 +0530

    Chatterbot Courpus Added

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+from copyreg import constructor
 import eel
 import ctypes
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import requests
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+   
+    database_uri='sqlite:///database.sqlite3'
+)
 
+trainer = ChatterBotCorpusTrainer(bot)
+trainer.train("chatterbot.corpus.english")
 
-print(screensize)
+# print(screensize)
 
 
 eel.init("web")
 
 
+def returnBotResponse(text):
+    return bot.get_response(text).text
 
 
+@eel.expose
+def returnResponse(input):
+    return returnBotResponse(input)
+
+@eel.expose
+def wolframeAlphaResponse(input):
+    request = requests.get("https://api.wolframalpha.com/v1/result?appid=G3PE54-VKYR55XQ2J&i="+input)
+    return request.text
 
 eel.start("index.html", size=screensize)
